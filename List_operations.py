c796f5c1d4af07a512f2d97807b8751776bb2541
Sitaram Ryali <45493988+SitaramRyali@users.noreply.github.com>
2019-06-14 13:33:39 +0530

    Update to easy solutions

--- List_operations.py
+++ List_operations.py
@@ -1,27 +1,15 @@
 if __name__ == '__main__':
-    N = int(input())
-
-ins = 'insert'
-rem = 'remove'
-appen = 'append'
-outlist=[]
-
-for i in range(N):
-    data = input().split() 
-    if(ins in data[0] ):
-        outlist.insert(int(data[-2]),int(data[-1]))
-    elif(appen in data[0] ):
-        outlist.append(int(data[-1]))    
-    elif(rem in data[0] ):
-        outlist.remove(int(data[-1]))
-    elif('sort' == data[0] ):
-        outlist.sort()
-    elif('reverse' == data[0] ):
-        outlist.reverse()
-    elif('pop' == data[0] ):
-        outlist.pop()
+n = int(input())
+l = []
+for _ in range(n):
+    s = input().split()
+    cmd = s[0]
+    args = s[1:]
+    if cmd !="print":
+        cmd += "("+ ",".join(args) +")"
+        eval("l."+cmd)
     else:
-        print(outlist)
+        print(l)
         
         
         
